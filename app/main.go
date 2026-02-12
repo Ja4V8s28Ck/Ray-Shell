1e3baf8005df48a6efcd61f4cc442d9722e248d3
Ja4V8s28Ck <66721411+Ja4V8s28Ck@users.noreply.github.com>
2026-02-12 19:04:07 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -12,11 +12,27 @@ func logFatal(err error) {
 	os.Exit(1)
 }
 
+func invalidCommand(shellCmd string) {
+	fmt.Println(shellCmd + ": command not found")
+}
+
 func main() {
 	// Map commands to it's function
-	cmdFuncMap := map[string]func(shellArgs []string){
-		"exit": func(shellArgs []string) { os.Exit(0) },
+	var cmdFuncMap map[string]func(shellArgs []string)
+	cmdFuncMap = map[string]func(shellArgs []string){
 		"echo": func(shellArgs []string) { fmt.Println(strings.Join(shellArgs, " ")) },
+		"exit": func(shellArgs []string) { os.Exit(0) },
+		"type": func(shellArgs []string) {
+			if len(shellArgs) == 0 {
+				fmt.Println("type command needs argument")
+			} else if len(shellArgs) == 1 {
+				if _, ok := cmdFuncMap[shellArgs[0]]; ok {
+					fmt.Println(shellArgs[0] + " is a shell builtin")
+				} else {
+					fmt.Println(shellArgs[0] + ": not found")
+				}
+			}
+		},
 	}
 
 	buffReader := bufio.NewReader(os.Stdin)
@@ -30,7 +46,7 @@ func main() {
 		}
 
 		var shellArgs []string
-		cmdLineArr := strings.Split(cmdLine[:len(cmdLine)-1], " ")
+		cmdLineArr := strings.Fields(cmdLine[:len(cmdLine)-1])
 		shellCmd := cmdLineArr[0]
 		if len(cmdLineArr) > 1 {
 			shellArgs = cmdLineArr[1:]
