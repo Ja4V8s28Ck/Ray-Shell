e9dd9a618058a9369abba203a1f9347b8c23f717
Ja4V8s28Ck <66721411+Ja4V8s28Ck@users.noreply.github.com>
2026-02-14 14:55:47 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -19,11 +19,16 @@ func main() {
 	cmdFuncMap = map[string]func(shellArgs []string){
 		"cd": func(shellArgs []string) {
 			if len(shellArgs) == 1 { // Only 1 arg allowed
+
+				if shellArgs[0] == "~" || strings.HasPrefix(shellArgs[0], "~/") { // convert relative path to abs path
+					shellArgs[0] = strings.Replace(shellArgs[0], "~", os.Getenv("HOME"), 1)
+				}
 				if err := os.Chdir(shellArgs[0]); err != nil {
 					fmt.Printf("cd: %s: No such file or directory\n", shellArgs[0])
 				}
+
 			} else {
-				fmt.Println("cd: too many argument")
+				fmt.Println("cd: too many arguments")
 			}
 		},
 		"echo": func(shellArgs []string) { fmt.Println(strings.Join(shellArgs, " ")) },
