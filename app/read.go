ccb9a5ba1d65e3bd33b5118e38d2c3c36f42529b
Ja4V8s28Ck <66721411+Ja4V8s28Ck@users.noreply.github.com>
2026-02-15 23:02:28 +0530

    codecrafters submit [skip ci]

--- app/read.go
+++ app/read.go
@@ -15,11 +15,53 @@ func readCmd(buffReader *bufio.Reader) (string, []string) {
 	}
 
 	var shellArgs []string
-	cmdLineArr := strings.Fields(cmdLine[:len(cmdLine)-1])
+	cmdLine = strings.TrimRight(cmdLine, "\n")
+	cmdLineArr := parseArgs(cmdLine)
 	shellCmd := cmdLineArr[0]
+
 	if len(cmdLineArr) > 1 {
 		shellArgs = cmdLineArr[1:]
 	}
 
 	return shellCmd, shellArgs
+}
+
+func parseArgs(input string) []string {
+	var args []string
+	var stringBuilder strings.Builder
+	var inQuote rune // 0 means not in quote, '"' or '\'' means in that quote
+
+	for i := 0; i < len(input); i++ {
+		char := rune(input[i])
+
+		switch {
+		case char == '"' || char == '\'':
+			if inQuote == 0 {
+				inQuote = char
+			} else if inQuote == char {
+				inQuote = 0
+			} else {
+				stringBuilder.WriteRune(char)
+			}
+
+		case char == ' ' && inQuote == 0:
+			if stringBuilder.Len() > 0 {
+				args = append(args, stringBuilder.String())
+				stringBuilder.Reset()
+			}
+
+		case char == '\\' && i+1 < len(input):
+			i++
+			stringBuilder.WriteByte(input[i])
+
+		default:
+			stringBuilder.WriteRune(char)
+		}
+	}
+
+	if stringBuilder.Len() > 0 {
+		args = append(args, stringBuilder.String())
+	}
+
+	return args
 }
