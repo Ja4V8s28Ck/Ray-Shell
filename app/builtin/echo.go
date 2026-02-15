f9e64b1f457f2df676857c1ef1c242d6139e8a59
Ja4V8s28Ck <66721411+Ja4V8s28Ck@users.noreply.github.com>
2026-02-15 23:03:54 +0530

    refactor: remove duplicate parsing function

--- app/builtin/echo.go
+++ app/builtin/echo.go
@@ -12,51 +12,5 @@ func (echo Echo) Name() string {
 }
 
 func (echo Echo) Execute(shellArgs []string) {
-	// fmt.Println(shellArgs)
 	fmt.Println(strings.Join(shellArgs, " "))
-	// fmt.Println(parseEchoArgs(strings.Join(shellArgs, " ")))
-}
-
-func parseEchoArgs(shellArgsString string) string {
-	shellArgsString = strings.TrimSpace(shellArgsString)
-
-	stringBuilder := strings.Builder{}
-	n := len(shellArgsString)
-
-	isSpace := false
-	var quotes rune
-
-	for i := 0; i < n; {
-		currChar := rune(shellArgsString[i])
-
-		switch currChar {
-		case '\\':
-			if quotes == '\'' {
-				stringBuilder.WriteRune(currChar)
-			}
-			stringBuilder.WriteRune(rune(shellArgsString[i+1]))
-			i++
-		case '"':
-		case '\'':
-			if quotes == currChar {
-				quotes = 0
-			} else if quotes == 0 {
-				quotes = currChar
-			} else {
-				stringBuilder.WriteRune(currChar)
-			}
-		case ' ':
-			if quotes != 0 || !isSpace {
-				stringBuilder.WriteRune(currChar)
-			}
-			isSpace = true
-
-		default:
-			stringBuilder.WriteRune(currChar)
-			isSpace = false
-		}
-
-		i++
-	}
-	return stringBuilder.String()
 }
