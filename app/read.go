46acffa9f4ea6dd93b6981e498b7bc7e2756431b
Ja4V8s28Ck <66721411+Ja4V8s28Ck@users.noreply.github.com>
2026-02-20 12:25:57 +0530

    feat: add readCmd function to read rawbytes from terminal

---
+++ app/read.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/term"
+)
+
+func readLine(prompt string) (string, error) {
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error initalizing xterm")
+		os.Exit(1)
+	}
+	defer term.Restore(int(os.Stdin.Fd()), oldState)
+
+	fmt.Print(prompt + " ")
+	var readBuffer []byte
+
+	cursorPtr := 0
+
+	for {
+		byteBuffer := make([]byte, 1)
+		os.Stdin.Read(byteBuffer)
+
+		switch byteBuffer[0] {
+
+		case 0x03:
+			return "", fmt.Errorf("^C")
+
+		case 0x7f:
+			if cursorPtr > 0 {
+				readBuffer = append(readBuffer[:cursorPtr-1], readBuffer[cursorPtr:]...)
+				cursorPtr--
+				redraw(prompt, readBuffer)
+			}
+
+		case '\r':
+			fmt.Print("\r\n")
+			return string(readBuffer), nil
+
+		default:
+			readBuffer = append(readBuffer[:cursorPtr], append(byteBuffer, readBuffer[cursorPtr:]...)...)
+			cursorPtr++
+			fmt.Print(string(byteBuffer))
+
+		}
+	}
+}
+
+func redraw(prompt string, readBuffer []byte) {
+	fmt.Printf("\r\x1b[K%s %s", prompt, string(readBuffer))
+}
