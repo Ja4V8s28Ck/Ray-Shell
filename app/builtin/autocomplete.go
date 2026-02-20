47d6eb58db6047347be1b80983307286cc385797
Ja4V8s28Ck <66721411+Ja4V8s28Ck@users.noreply.github.com>
2026-02-20 18:16:19 +0530

    feat: implement trie for autocompletion

---
+++ app/builtin/autocomplete.go
@@ -0,0 +1,68 @@
+package builtin
+
+type TrieNode struct {
+	children  map[rune]*TrieNode
+	endOfWord bool
+}
+
+func Trie() TrieNode {
+	return TrieNode{
+		children: make(map[rune]*TrieNode),
+	}
+}
+
+func (T *TrieNode) insertWord(word string) {
+	currentNode := T
+
+	for _, char := range word {
+		if _, ok := currentNode.children[char]; !ok {
+			currentNode.children[char] = &TrieNode{children: make(map[rune]*TrieNode)}
+		}
+		currentNode = currentNode.children[char]
+	}
+
+	currentNode.endOfWord = true
+}
+
+func (T *TrieNode) findAllMatches(prefixString string) []string {
+	currentNode := T
+
+	// find the trieNode at the end of prefix string
+	for _, char := range prefixString {
+		if _, ok := currentNode.children[char]; !ok {
+			return nil
+		}
+		currentNode = currentNode.children[char]
+	}
+
+	var matchedStrings []string
+
+	// dfs the whole word set for matches
+	var dfs func(T *TrieNode, currentString string)
+
+	dfs = func(T *TrieNode, currentString string) {
+		if T.endOfWord {
+			matchedStrings = append(matchedStrings, currentString)
+		}
+		for k, childNode := range T.children {
+			dfs(childNode, currentString+string(k))
+		}
+	}
+
+	dfs(currentNode, "")
+
+	return matchedStrings
+}
+
+var trie TrieNode = Trie()
+
+func buildTrie() {
+	for key := range CmdFuncMap {
+		trie.insertWord(key)
+	}
+}
+
+func AutoComplete(prefixString string) []string {
+	matchedStrings := trie.findAllMatches(prefixString)
+	return matchedStrings
+}
