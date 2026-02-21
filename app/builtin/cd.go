6417bc8514da0220d7624485cc6e95b31d1c63c4
Ja4V8s28Ck <66721411+Ja4V8s28Ck@users.noreply.github.com>
2026-02-21 20:49:28 +0530

    refactor(builtin/cd): handle case where no argument is given

--- app/builtin/cd.go
+++ app/builtin/cd.go
@@ -13,16 +13,24 @@ func (cd Cd) Name() string {
 }
 
 func (cd Cd) Execute(shellArgs []string, ctx *ExecContext) {
-	if len(shellArgs) == 1 { // Only 1 arg allowed
+	shellArgsCount := len(shellArgs)
 
-		if shellArgs[0] == "~" || strings.HasPrefix(shellArgs[0], "~/") { // convert relative path to abs path
-			shellArgs[0] = strings.Replace(shellArgs[0], "~", os.Getenv("HOME"), 1)
-		}
-		if err := os.Chdir(shellArgs[0]); err != nil {
-			fmt.Fprintf(ctx.Stderr, "cd: %s: No such file or directory\n", shellArgs[0])
-		}
-
-	} else {
+	if shellArgsCount > 1 { // Only 1 arg allowed
 		fmt.Fprintln(ctx.Stderr, "cd: too many arguments")
+		return
+	}
+
+	if shellArgsCount == 0 {
+		shellArgs = append(shellArgs, "/")
+	}
+
+	cdArg := shellArgs[0]
+
+	if cdArg == "~" || strings.HasPrefix(cdArg, "~/") { // convert relative path to abs path
+		cdArg = strings.Replace(cdArg, "~", os.Getenv("HOME"), 1)
+	}
+
+	if err := os.Chdir(cdArg); err != nil {
+		fmt.Fprintf(ctx.Stderr, "cd: %s: No such file or directory\n", cdArg)
 	}
 }
